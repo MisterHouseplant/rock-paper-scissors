190d0928916aa528834ef7e1b65fafede552e18c
Sara <122331792+MisterHouseplant@users.noreply.github.com>
2023-11-15 16:23:27 -0500

    remove comments Removed comments at the beginning with the instructions. Those are in the README file, didn't want redundant info cluttering the code.

--- rps.js
+++ rps.js
@@ -1,19 +1,4 @@
-//assignment notes:
-// game will be played against computer, so start with 
-// function called getComputerChoice that will randomly 
-//generate either "Rock", "Paper", or "Scissors". This function
-// will make the computer's play. 
-//(check in console to make sure this is working  before 
-//continuing to the next step.
 
-//Write a function that plays a single round of RPS.
-//The function should take 2 parameters:
-//playerSelection and
-//computerSelection-and then return a string that declares 
-// the winner of the round like so "You Lose! Paper beats Rock"
-//Make your function’s playerSelection parameter 
-//case-insensitive (so users can input rock, ROCK, 
- //   RocK or any other variation).
 //---------------------------------------------------------------
 //Solution plan:
 //Rock beats scissors, scissors beat paper, and paper beats 
